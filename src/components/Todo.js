f4cd4e179e1795975f0935c39d6c6caab791939a
Chinh Le <chinh.le11@gmail.com>
2019-06-11 14:42:07 -0400

    adding array destructuring

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -9,18 +9,22 @@ const todo = props => {
       *         [1]: function can be used to manipulate the state
      */
     // eslint-disable-next-line react-hooks/rules-of-hooks
-    const inputState = useState('someValue');
+    const [todoName, setTodoName] = useState('someValue');
+    // console.log('todoName: ', todoName);
+    // const inputState = useState('someValue');
     // console.log('inputState: ', inputState);
 
     const inputChangeHandler = (evt) => {
         // console.log('evt: ', evt.target.value);
-        inputState[1](evt.target.value);
+        setTodoName(evt.target.value);
+        // inputState[1](evt.target.value);
     };
 
     return (
         // React.Fragment: allows top level siblings 
         <React.Fragment>
-            <input type="text" placeholder="Todo" onChange={inputChangeHandler} value={inputState[0]} /> {/* functional component */}
+            <input type="text" placeholder="Todo" onChange={inputChangeHandler} value={todoName} /> {/* functional component */}
+            {/* <input type="text" placeholder="Todo" onChange={inputChangeHandler} value={inputState[0]} /> */} {/* functional component */}
             {/* <input type="text" placeholder="Todo" onChange={this.inputUserHandler} value={this.state.todoValue} /> */} {/* class-based component */}
             <button type="button">Add</button>
             <ul />
